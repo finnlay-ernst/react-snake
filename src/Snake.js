ce3653988dba91f1ccdda4c1f57ce86092b7c60f
Finnlay Ernst <fernst14@gmail.com>
2020-07-03 13:56:18 +1000

    Created game grid with css

--- src/Snake.js
+++ src/Snake.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Snake.css';
 
+const gameWidth = 8;
+
+let widthArray = [];
+for (let i = 0; i<gameWidth; i++)
+  widthArray.push(i);  
+
 function Snake() {
   return (
     <div className="SiteContainer">
@@ -14,9 +20,22 @@ function Snake() {
 function Header() {
   return <h1 className="Heading">React Snake</h1>; 
 }
+
 function Game() {
-  return <div className="GameContainer">Placeholder</div>; 
+  return <div className="GameContainer">
+    {
+      widthArray.map((item, i) => <Row width={gameWidth}/>)
+    }
+  </div>; 
 }
+function Row({ width }){
+  return <div className="Row" style={{height: `${500/gameWidth}px`}}>
+    {
+      widthArray.map((item, i) => <div className="Cell" key={i}></div>)
+    }
+  </div>;
+}
+
 function Footer() {
   return <footer className="Footer">By Finnlay Ernst | finnlay.ernst@gmail.com</footer>; 
 }
