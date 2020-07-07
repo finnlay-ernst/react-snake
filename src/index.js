d55b44ceb103db3e75106e1ed389a9eb787fb023
Finnlay Ernst <fernst14@gmail.com>
2020-07-07 20:13:44 +1000

    CSS Tweaks

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+  
+document.title = 'React Snake';
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
